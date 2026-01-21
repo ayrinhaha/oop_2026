353fe860cdb3189a6ca836466d4fc0afe665d21f
ayrinhaha <irenemae.pingol@lorma.edu>
2026-01-21 11:51:30 +0800

    act1

---
+++ activity1/Book.java
@@ -0,0 +1,18 @@
+package activity1;
+
+class Book {
+
+    String title;
+    String author;
+    String isbn;
+    String publicationDate;
+
+    void printBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Publication date: " + publicationDate);
+
+    }
+
+}
---
+++ activity1/Main.java
@@ -0,0 +1,25 @@
+package activity1;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.title = "To Kill a Mockingbird";
+        book1.author = "Harper Lee";
+        book1.isbn = "9780060935467"; 
+        book1.publicationDate = "July 11, 1960";
+
+        Book book2 = new Book();
+        book2.title = "Fire & Blood";
+        book2.author = "George R.R. Martin";
+        book1.isbn = "9781524796280";
+        book2.publicationDate = "November 20, 2018";
+        
+        
+        
+        
+
+    }
+
+
+    
+}
